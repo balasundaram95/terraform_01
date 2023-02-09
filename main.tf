7a68496c36a73231c23d0ee89d315c845494b6c2
silcontechlabs <120162057+silcontechlabs@users.noreply.github.com>
2023-02-09 19:38:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -68,13 +68,13 @@ terraform {
 }
 
 #resource block
-resource "aws_instance" "windows" {
-  ami           = "ami-09e8627bd5d36a4f0"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = ["sg-006c39b7fb99756f9"]
-  key_name = "test_chrome"
+# resource "aws_instance" "windows" {
+#   ami           = "ami-09e8627bd5d36a4f0"
+#   instance_type = "t2.micro"
+#   vpc_security_group_ids = ["sg-006c39b7fb99756f9"]
+#   key_name = "test_chrome"
 
-  tags = {
-    Name = "Test_chrome_final1"
-  }
+#   tags = {
+#     Name = "Test_chrome_final1"
+#   }
 }
