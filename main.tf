80dc9efcfb2258a73c1e8b2d3301ae94f4c273e7
balasundaram95 <121680920+balasundaram95@users.noreply.github.com>
2023-02-13 13:19:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -67,14 +67,15 @@ terraform {
 
 }
 
-resource block
+#resource block
 resource "aws_instance" "windows" {
-ami           = "ami-09e8627bd5d36a4f0"
-instance_type = "t2.micro"
-vpc_security_group_ids = ["sg-006c39b7fb99756f9"]
-key_name = "test_chrome"
+  ami           = "ami-09e8627bd5d36a4f0"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-006c39b7fb99756f9"]
+  key_name = "test_chrome"
 
-tags = {
+  tags = {
     Name = "Test_chrome_final1"
-       }
+  }
 }
+
