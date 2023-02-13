42d51bfcf63c32611ef35ceeb2ef0c5d2f1bab4d
balasundaram95 <121680920+balasundaram95@users.noreply.github.com>
2023-02-13 17:33:48 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -24,11 +24,10 @@ terraform {
 
 
 
-
 provider "aws" {
   region     = "us-east-1"
-  access_key = secrets.AWS_ACCESS_KEY_ID
-  secret_key = secrets.AWS_SECRET_KEY_ID 
+  access_key = "AKIAXZ3PKUV4MWXBZV2P"
+  secret_key = "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"
 }
 
 
